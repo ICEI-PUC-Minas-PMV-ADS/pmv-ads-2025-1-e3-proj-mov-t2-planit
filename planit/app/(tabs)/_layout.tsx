840749fbaa1e08c30266d61b59942690a852e175
maraiii <mariaeduantunes200281@gmail.com>
2025-04-10 15:34:12 -0300

    Barra de navegação - BottomNav

--- planit/app/(tabs)/_layout.tsx
+++ planit/app/(tabs)/_layout.tsx
@@ -1,45 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Entypo, Ionicons, MaterialIcons, Octicons } from '@expo/vector-icons'
+import { Colors } from '../../constants/Colors'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: 'Home',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <Ionicons name='home' size={16} color={Colors.principal} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="Calendário"
+            options={{
+                title: 'Calendário',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <Entypo name="calendar" size={16} color={Colors.iconNavDefault} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="Clientes"
+            options={{
+                title: 'Clientes',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <Octicons name="people" size={16} color={Colors.iconNavDefault} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="Serviços"
+            options={{
+                title: 'Serviços',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <MaterialIcons name="home-repair-service" size={16} color={Colors.iconNavDefault} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="Perfil"
+            options={{
+                title: 'Perfil',
+                headerShown: false,
+                tabBarIcon: ({color, size}) => (
+                    <MaterialIcons name="person-outline" size={16} color={Colors.iconNavDefault} />
+                ),
+            }}
+        />
     </Tabs>
-  );
+    
+  )
 }
+
+export default _layout
