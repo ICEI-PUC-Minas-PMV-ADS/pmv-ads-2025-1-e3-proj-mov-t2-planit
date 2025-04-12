3919c3e6883b24b730ea54b33a9aff45c9807d1d
Emily Avelino <140619083+emy2806@users.noreply.github.com>
2025-04-12 19:39:54 -0300

    Teste tailwind + components

---
+++ planit/components/calendar/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import '../../styles/global.css'
+
+export function Calendar() {
+    return (
+        <View className="container bg-principal shadow-zinc-800">
+            
+        </View>
+    )
+}
+
