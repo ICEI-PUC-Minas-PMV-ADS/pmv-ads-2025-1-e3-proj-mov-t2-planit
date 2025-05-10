0aa30679b641f4a835c40ce52dc138a6059a7606
maraiii <mariaeduantunes200281@gmail.com>
2025-05-10 12:47:27 -0300

    conexão bd web

--- planit-web/src/App.tsx
+++ planit-web/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    getDocs(collection(db, "Usuario"))
+      .then((snap) => snap.docs.map((d) => d.data()))
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
