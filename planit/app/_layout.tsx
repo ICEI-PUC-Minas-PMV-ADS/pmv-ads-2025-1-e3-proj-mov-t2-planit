c58db700f07ba12378984ee49ffebd33116709d3
maraiii <mariaeduantunes200281@gmail.com>
2025-05-12 18:02:38 -0300

    Pop-up da página de notificações

--- planit/app/_layout.tsx
+++ planit/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
+import { MenuProvider } from "react-native-popup-menu";
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="loginGoogle" options={{ headerShown: false }} />
-      <Stack.Screen name="Login" options={{ headerShown: false }} />
-      <Stack.Screen name="Register" options={{ headerShown: false }} />
-    </Stack>
+    <MenuProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="loginGoogle" options={{ headerShown: false }} />
+        <Stack.Screen name="Login" options={{ headerShown: false }} />
+        <Stack.Screen name="Register" options={{ headerShown: false }} />
+      </Stack>
+    </MenuProvider>
   );
 }
