7628ff0b72f8feade4fe84e654a4aa80bb6eb16f
Emily Avelino <140619083+emy2806@users.noreply.github.com>
2025-05-15 11:25:12 -0300

    pages - PLANIT_WEB

---
+++ planit-web/src/pages/SelectDataHora.tsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function SelectDataHora() {
+
+  return (
+    <div className='m-5 flex flex-col gap-10'>
+        <button  type='submit' className='p-1 w-26 h-12 rounded-xl border border-emerald-600  bg-emerald-500 cursor-pointer text-white hover:bg-white hover:text-emerald-600'>Teste</button>
+    </div>
+  )
+}
+
+export default SelectDataHora
