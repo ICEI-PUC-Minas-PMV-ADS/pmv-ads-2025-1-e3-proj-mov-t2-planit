da26c9eb828df3c464b0a1c19b2ba7ae753a6699
Emily Avelino <140619083+emy2806@users.noreply.github.com>
2025-05-20 19:01:39 -0300

    Cadastro - PLANIT_WEB

--- planit-web/src/pages/Cadastro.tsx
+++ planit-web/src/pages/Cadastro.tsx
@@ -1,11 +1,89 @@
 import '../App.css'
 import { useNavigate } from 'react-router-dom';
+
+import { useState } from 'react';
 import { IonIcon } from '@ionic/react';
 import { eyeOutline } from 'ionicons/icons';
 import PinkBtn from '../components/pinkBtn';
 
+import { collection, addDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../firebaseConfig';
+
+
 function Cadastrar() {
     const navigate = useNavigate();
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confirmarSenha, setConfirmarSenha] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const isValidEmail = (email: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    const handleCadastro = async () => {
+        if (!nome || !email || !senha || !confirmarSenha) {
+            window.alert('Preencha todos os campos.');
+            return;
+        }
+
+        if (!isValidEmail(email)) {
+            window.alert('Por favor, insira um email válido.');
+            return;
+        }
+
+        if (senha.length < 8) {
+            window.alert('A senha deve ter pelo menos 8 caracteres.');
+            return;
+        }
+
+        if (senha !== confirmarSenha) {
+            window.alert('As senhas não coincidem.');
+            return;
+        }
+
+        setIsLoading(true);
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
+            const user = userCredential.user;
+
+            await addDoc(collection(db, 'Usuario'), {
+                uid: user.uid,
+                nome,
+                email,
+                criadoEm: new Date(),
+            });
+
+            window.alert('Usuário cadastrado com sucesso!');
+            navigate('/home');
+
+        } catch (error) {
+            if (error instanceof Error && 'code' in error) {
+                const firebaseError = error as { code: string };
+
+                if (firebaseError.code === 'auth/email-already-in-use') {
+                    window.alert('Este email já está em uso.');
+
+                } else if (firebaseError.code === 'auth/invalid-email') {
+                    window.alert('Email inválido.');
+
+                } else {
+                    window.alert('Ocorreu um erro ao cadastrar. Tente novamente.');
+                }
+
+            } else {
+                window.alert('Erro desconhecido.');
+
+            }
+            console.error('Erro ao cadastrar:', error);
+
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <div>
@@ -24,11 +102,20 @@ function Cadastrar() {
                     <div className='flex flex-col gap-5 justify-center'>
                         <div className='flex gap-2 items-center'>
                             <div>
+                                <label>Nome:</label>
+                            </div>
+
+                            <div>
+                                <input onChange={(n) => setNome(n.target.value)} value={nome} type="text" className='border border-gray-700 p-2 w-56 rounded-xl  outline-none' />
+                            </div>
+                        </div>
+                        <div className='flex gap-2 items-center'>
+                            <div>
                                 <label>E-mail:</label>
                             </div>
 
                             <div>
-                                <input type="text" className='border border-gray-700 p-2 w-52 rounded-xl  outline-none' />
+                                <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" className='border border-gray-700 p-2 w-56 rounded-xl  outline-none' />
                             </div>
                         </div>
 
@@ -39,7 +126,23 @@ function Cadastrar() {
 
                             <div className='flex gap-2 items-center'>
                                 <div>
-                                    <input type="password" className='outline-none border border-gray-700 p-2 w-52 rounded-xl' />
+                                    <input onChange={(s) => setSenha(s.target.value)} value={senha} type="password" className='outline-none border border-gray-700 p-2 w-56 rounded-xl' />
+                                </div>
+
+                                <div>
+                                    <IonIcon icon={eyeOutline} className='cursor-pointer' />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex gap-2 items-center'>
+                            <div>
+                                <label>Confirmar senha:</label>
+                            </div>
+
+                            <div className='flex gap-2 items-center'>
+                                <div>
+                                    <input onChange={(cs) => setConfirmarSenha(cs.target.value)} type="password" className='outline-none border border-gray-700 p-2 w-46 rounded-xl' />
                                 </div>
 
                                 <div>
@@ -54,7 +157,14 @@ function Cadastrar() {
                     </div>
 
                     <div className='flex justify-center'>
-                        <PinkBtn title='Cadastrar' onClick={() => navigate('/agendar')} />
+                        <PinkBtn title='Cadastrar' onClick={handleCadastro} disabled={isLoading}> 
+                            { isLoading ? (
+                                <PinkBtn/>
+                            ) : (
+                                <PinkBtn title='Cadastrar'/>
+                            )}
+                        </PinkBtn>
+                        
                     </div>
                 </form>
             </div>
